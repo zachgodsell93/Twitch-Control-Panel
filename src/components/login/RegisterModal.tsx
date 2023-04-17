04fb89905e32e7dcc80db89cc3f4473be1f10de1
Zach Godsell <zacharyjamesgodsell@gmail.com>
2023-04-17 20:40:54 +1000

    update

--- src/components/login/RegisterModal.tsx
+++ src/components/login/RegisterModal.tsx
@@ -1,11 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const RegisterModal: React.FC<RegisterProps> = (props) => {
+interface RegisterProps {
+	modalOpen: (data: boolean) => void;
+}
+
+const RegisterModal: React.FC<RegisterProps> = (props) => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const checkPassword = (password: string, password2: string) => {
+		if (password.length <= 8) {
+			if (password === password2) {
+				if (
+					password.match(/[a-z]/) &&
+					password.match(/[A-Z]/) &&
+					password.match(/\d/)
+				) {
+				} else {
+					console.log(
+						"Password must contain an uppercase, lowercase and a number"
+					);
+				}
+			} else {
+				console.log("Passwords do not match");
+			}
+		} else {
+			console.log("Password needs to be at least 8 characters long");
+		}
+	};
+
+	const handleUserRegistration = () => {};
+
+	const closeModal = () => {
+		props.modalOpen(false);
+	};
 	return (
-		<div className="w-[500px] bg-white border shadow-md filter opacity-100 visible">
-			RegisterModal
-		</div>
+		<section className="relative w-full h-full min-h-screen z-20">
+			<div className="absolute top-0 w-full h-full bg-slate-700 bg-no-repeat bg-full"></div>
+			<div className="container mx-auto px-4 h-full bg-slate-700">
+				<div className="flex content-center items-center justify-center h-full">
+					<div className="w-full lg:w-4/12 px-4">
+						<div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-slate-200 border-0">
+							<div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+								<div className="text-slate-400 text-center mb-3 font-bold pt-8">
+									<small>Register</small>
+								</div>
+								<div className="relative w-full mb-3">
+									<label
+										className="block uppercase text-slate-600 text-xs font-bold mb-2"
+										htmlFor="grid-password"
+									>
+										Email
+									</label>
+									<input
+										name="email"
+										id="email"
+										type="email"
+										className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+										placeholder="Email"
+										onChange={(e) => setEmail(e.target.value)}
+									/>
+								</div>
+
+								<div className="relative w-full mb-3">
+									<label
+										className="block uppercase text-slate-600 text-xs font-bold mb-2"
+										htmlFor="grid-password"
+									>
+										Password
+									</label>
+									<input
+										name="password"
+										id="password"
+										type="password"
+										className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+										placeholder="Password"
+										onChange={(e) => setPassword(e.target.value)}
+									/>
+								</div>
+
+								<div className="relative w-full mb-3">
+									<label
+										className="block uppercase text-slate-600 text-xs font-bold mb-2"
+										htmlFor="grid-password"
+									>
+										Re-type Password
+									</label>
+									<input
+										type="password"
+										name="password2"
+										id="password2"
+										className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+										placeholder="Re-enter Password"
+										onChange={(e) => setPassword(e.target.value)}
+									/>
+								</div>
+
+								<div className="text-center mt-6">
+									<button
+										className="bg-slate-800 text-white active:bg-slate-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+										type="submit"
+										onClick={handleUserRegistration}
+									>
+										Register
+									</button>
+								</div>
+								<div className="text-center mt-6">
+									<button
+										className="bg-slate-800 text-white active:bg-slate-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+										onClick={closeModal}
+									>
+										Cancel
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 	);
 };
 
-type RegisterProps = {};
+export { RegisterModal };
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { RegisterModal } from "../components/login/RegisterModal";
 
 export const Login = (props?: LoginPropTypes) => {
 	const [incorrectPassword, setIncorrectPassword] = useState(false);
+	const [register, setRegister] = useState(false);
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+
+	const handleCloseRegister = (data: boolean) => {
+		setRegister(false);
+	};
 
 	const navigate = useNavigate();
 	const handleSubmit = () => {
@@ -26,6 +32,7 @@ export const Login = (props?: LoginPropTypes) => {
 
 	return (
 		<section className="relative w-full h-full py-40 min-h-screen">
+			{register ? <RegisterModal modalOpen={handleCloseRegister} /> : null}
 			<div className="absolute top-0 w-full h-full bg-slate-700 bg-no-repeat bg-full"></div>
 			<div className="container mx-auto px-4 h-full bg-slate-700">
 				<div className="flex content-center items-center justify-center h-full">
@@ -33,7 +40,7 @@ export const Login = (props?: LoginPropTypes) => {
 						<div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-slate-200 border-0">
 							<div className="flex-auto px-4 lg:px-10 py-10 pt-0">
 								<div className="text-slate-400 text-center mb-3 font-bold pt-8">
-									<small>Layback and Win Bot Login</small>
+									<small>Dev Dojo Twitch Panel</small>
 								</div>
 								{incorrectPassword ? (
 									<div className="py-2 mb-4 rounded-lg text-center bg-red-300 w-full">
@@ -68,13 +75,14 @@ export const Login = (props?: LoginPropTypes) => {
 									</label>
 									<input
 										type="password"
-										name="password"
-										id="password"
+										name="password1"
+										id="password1"
 										className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
 										placeholder="Password"
 										onChange={(e) => setPassword(e.target.value)}
 									/>
 								</div>
+
 								<div>
 									<label className="inline-flex items-center cursor-pointer">
 										<input
@@ -97,17 +105,6 @@ export const Login = (props?: LoginPropTypes) => {
 										Sign In
 									</button>
 								</div>
-								<div className="relative w-full mb-3 pt-10">
-									<label
-										className="block uppercase text-slate-600 text-xs font-bold mb-2"
-										htmlFor="grid-password"
-									>
-										Don't have an account?
-									</label>
-									<button className="bg-slate-800 text-white active:bg-slate-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150">
-										Register Now
-									</button>
-								</div>
 							</div>
 						</div>
 						<div className="flex flex-wrap mt-6 relative">
@@ -117,9 +114,12 @@ export const Login = (props?: LoginPropTypes) => {
 								</a>
 							</div>
 							<div className="w-1/2 text-right">
-								<Link to="/" className="text-slate-200">
-									<small>Create new account</small>
-								</Link>
+								<small
+									className="cursor-pointer"
+									onClick={() => setRegister(true)}
+								>
+									Create new account
+								</small>
 							</div>
 						</div>
 					</div>
